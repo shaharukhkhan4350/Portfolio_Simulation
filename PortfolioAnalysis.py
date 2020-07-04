6c36ed4d1a3d351ba8155608d827e048412daad3
Shaharukh Khan <shaharukhkhan4350@gmail.com>
2020-07-04 14:24:50 +0530

    Initial changes

--- PortfolioAnalysis.py
+++ PortfolioAnalysis.py
@@ -31,7 +31,7 @@ class PortFolAnalysis:
             results[2,i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
         #return results, weights_cache
         params, rand_portf  = results, weights_cache
-        print(len(rand_portf), len(params))
+        #print(len(rand_portf), len(params))
         idx_min_vol = params[0].argmin()
         idx_max_retrn = params[1].argmax()
         idx_max_sharpe = params[2].argmax()
@@ -43,33 +43,39 @@ class PortFolAnalysis:
         print("Weigths of maximum sharpe portfolio are: ", max_sharpe_portf, "  and sharpe is: ", params[2][idx_max_sharpe])
         
         plt.figure(figsize=(16, 10))
-        plt.scatter(params[0], params[1], c = params[2], cmap = "YlGnBu", marker = "o", alpha=0.9)
+        plt.scatter(params[0], params[1], c = params[2], cmap = "YlGnBu", marker = "o",s=10, alpha=0.9)
         plt.ylabel("Return")
         plt.xlabel("Volatility")
+        plt.colorbar(label = "Sharpe ratio")
         #plt.plot( params[1][idx_max_retrn], params[0][idx_max_retrn], 'g')
-        plt.plot( params[0][idx_max_retrn],params[1][idx_max_retrn],  'g*')
-        plt.annotate("  Maximum Return",(params[0][idx_max_retrn], params[1][idx_max_retrn]))
-        plt.plot(params[0][idx_min_vol],params[1][idx_min_vol], 'r+')
-        plt.annotate("  Minimum Volatility",( params[0][idx_min_vol], params[1][idx_min_vol]))
-        plt.plot(params[0][idx_max_sharpe], params[1][idx_max_sharpe],'yo')
-        plt.annotate("  Maximum Sharpe",( params[0][idx_max_sharpe], params[1][idx_max_sharpe]))
+        plt.scatter( params[0][idx_max_retrn],params[1][idx_max_retrn], marker = "*" ,s = 500, color = 'g', label = "Maximum Return")
+        #plt.annotate("  Maximum Return",(params[0][idx_max_retrn], params[1][idx_max_retrn]))
+        plt.scatter(params[0][idx_min_vol],params[1][idx_min_vol],  marker = "*" , color = 'r', s=500, label = "Minimum Volatility")
+        #plt.annotate("  Minimum Volatility",( params[0][idx_min_vol], params[1][idx_min_vol]))
+        plt.scatter(params[0][idx_max_sharpe], params[1][idx_max_sharpe], marker = "*" , color = 'y', s=500, label = "Maximum Sharpe Ratio")
+        #plt.annotate("  Maximum Sharpe",( params[0][idx_max_sharpe], params[1][idx_max_sharpe]))
         plt.title("Visualization of Return vs. Volatility")
+        plt.legend(labelspacing=0.6)
         plt.savefig('Return vs volatility.png')
         
     def Optimal_weights(self, req_return):
         n = len(self.ticker)
         symbols = self.ticker
-        print(n)
+        #print(n)
         x = Variable(n)
         ret = (self.return_arr.T*x)*252 
         risk = quad_form(x, self.cov_matrix)*252
         prob = Problem(Minimize(risk), [sum(x)==1, ret >= req_return, x >= 0])
         prob.solve()
         print ("Optimal portfolio")
-        print ("----------------------")
+        print ("****************************************************************")
         for s in range(len(symbols)):
             print (" Investment in {} : {}% of the portfolio".format(symbols[s],round(100*x.value[s],2)))
-        print ("----------------------")
-        print ("Exp ret = {}%".format(round(100*ret.value,2)))
+        print ("****************************************************************")
+        print ("Expected return = {}%".format(round(100*ret.value,2)))
         print ("Expected risk    = {}%".format(round(100*risk.value**0.5,2)))
+        optim_std_dev, optim_return = PortFolAnalysis.__annual_performance(self, x.value)
+        print ("*****************************************************************")
+        print ("Volatility and return of optimized portfolio are: ", optim_std_dev, " and ", optim_return)
+        print ("Weights of optimal portfolio are: ", x.value)
         return x.value, risk.value
